e700c9e675f017637cbac8d49216a83b583d15f3
zourbuth <zourbuth@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-19 13:58:41 +0000

    Update to version 1.2.5
    git-svn-id: https://plugins.svn.wordpress.org/feedburner-email-subscription/tags/1.2.5@684223 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feedburner-email-subscription.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Feedburner Email Subscription
+ * 
+ * @package Zourbuth
+ * @subpackage Function 
+ * Give your biggest fans another way to keep up with your content feed by placing an email subscription widget on your site.
+ * This widget support shortcode for the short description.
+ * For another improvement, you can drop email to zourbuth@gmail.com or visit http://zourbuth.com/feedburner-email-subscription
+**/
+
+	
+/**
+ * Outputs the widget based on the arguments input through the widget controls.
+ * @since 0.6.0
+ */
+function proc_feedburner_email_subscription( $args ) { 
+	// Set up the default form values
+	$defaults = array(
+		'feed'		=> '',
+		'text'		=> __( 'Your email here', 'feedburner-email-subscription' ),
+		'submit'	=> __( 'Subscribe', 'feedburner-email-subscription' ),
+	);
+
+	// Merge the user-selected arguments with the defaults
+	$args = wp_parse_args( (array) $args, $defaults );
+	extract( $args );
+	?>
+	<form action="http://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('http://feedburner.google.com/fb/a/mailverify?uri=<?php echo $feed; ?>', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
+		<p>
+			<input type="text" value="<?php echo $text; ?>" onfocus="if(this.value=='<?php echo $text; ?>')this.value='';" onblur="if(this.value=='')this.value='<?php echo $text; ?>';" name="email">
+			<input class="btn" type="submit" name="imageField" value="<?php echo $submit; ?>" alt="Submit" />
+			<input type="hidden" value="<?php echo $feed; ?>" name="uri"/>
+			<input type="hidden" name="loc" value="en_US" />			
+		</p>
+	</form>
+	<?php
+		if ( !current_theme_supports('automatic-feed-links') )
+			return;
+
+		$rss = array(
+			/* translators: Separator between blog name and feed type in feed links */
+			'separator'	=> _x('&raquo;', 'feed link'),
+			/* translators: 1: blog title, 2: separator (raquo) */
+			'feedtitle'	=> __('%1$s %2$s Feed'),
+			/* translators: %s: blog title, 2: separator (raquo) */
+			'comstitle'	=> __('%1$s %2$s Comments Feed'),
+		);
+	
+		if( $args['posts_feed_link'] )
+			echo '<a class="feed-link" title="' . esc_attr(sprintf( $rss['feedtitle'], get_bloginfo('name'), $rss['separator'] )) . '" href="' . get_feed_link() . '"><img alt="i" src="' . FEEDBURNER_EMAIL_SUBSCRIPTION_URL . 'images/rss.png" />Posts <abbr title="Really Simple Syndication">RSS</abbr></a>';
+		
+		if( $args['comments_feed_link'] )
+			echo '<a class="feed-link" title="' . esc_attr(sprintf( $rss['comstitle'], get_bloginfo('name'), $rss['separator'] )) . '" href="' . get_feed_link( 'comments_' . get_default_feed() ) . '"><img alt="i" src="' . FEEDBURNER_EMAIL_SUBSCRIPTION_URL . 'images/rss.png" />Comments <abbr title="Really Simple Syndication">RSS</abbr></a>';
+	?>
+
+	<?php
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+ /*
+	Plugin Name: Feedburner Email Subscription
+	Plugin URI: http://zourbuth.com/archives/498/feedburner-email-subscription-wordpress-plugin/
+	Description: Give your biggest fans another way to keep up with your content feed by placing a <a href="http://www.feedburner.com/" alt="Feedburner" title="Feedburner">Feedburner</a> email subscription widget on your site. This widget will follow your theme stylesheet.
+	Version: 1.2.5
+	Author: zourbuth
+	Author URI: http://zourbuth.com
+	License: Under GPL2
+ 
+	Copyright 2013 zourbuth (email : zourbuth@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* Launch the plugin. */
+add_action( 'plugins_loaded', 'zframe_feedburner_email_subscription_plugins_loaded' );
+
+
+/**
+ * Initializes the plugin and it's features
+ * @since 1.0
+ */		
+function zframe_feedburner_email_subscription_plugins_loaded() {
+
+	// Set constant for feedburner plugin
+	define( 'FEEDBURNER_EMAIL_SUBSCRIPTION_VERSION', '1.2.5' );
+	define( 'FEEDBURNER_EMAIL_SUBSCRIPTION_DIR', plugin_dir_path( __FILE__ ) );
+	define( 'FEEDBURNER_EMAIL_SUBSCRIPTION_URL', plugin_dir_url( __FILE__ ) );
+	
+	// Require additional plugin file
+	require_once( FEEDBURNER_EMAIL_SUBSCRIPTION_DIR . 'feedburner-email-subscription.php' );
+	require_once( FEEDBURNER_EMAIL_SUBSCRIPTION_DIR . 'feedburner-email-subscription-shortcode.php' );	
+	
+	load_plugin_textdomain( 'feedburner-email-subscription', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+	// Loads and registers the new widgets
+	add_action( 'widgets_init', 'feedburner_email_subscription_widget_init' );
+}
+
+
+/**
+ * Register the extra widgets. Each widget is meant to replace or extend the current default
+ * @since 1.0
+ */
+function feedburner_email_subscription_widget_init() {
+	require_once( FEEDBURNER_EMAIL_SUBSCRIPTION_DIR . 'feedburner-email-subscription-widget.php' );
+	register_widget( 'Feedburner_Email_Subscription' );
+}
+
+?>
